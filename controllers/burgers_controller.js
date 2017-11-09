123a91c426a218628adb9867c5646dd6d0758474
Chelsey Dolan <chelseymarie6@gmail.com>
2017-11-08 23:05:27 -0800

    90 percent solution

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,7 +20,7 @@ router.post("/burgers/create", function(req, res){
     });
 });
 
-router.put("burgers/update/:id", function(req, res){
+router.put("/burgers/update/:id", function(req, res){
     burgers.update(req.body.devoured, req.params.id, function(){
         res.redirect("/burgers");
     });
