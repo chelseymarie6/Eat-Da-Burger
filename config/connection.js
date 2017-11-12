3b1e2f6e07269796bbc3d92a4f75ca71dd6aae4f
Chelsey Dolan <chelseymarie6@gmail.com>
2017-11-12 10:38:54 -0800

    final working

--- config/connection.js
+++ config/connection.js
@@ -10,8 +10,8 @@ if(process.env.JAWSDB_URL) {
   var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Yeslech#612",
     database: "burgers_db"
+    //password: "",
   });
 }
 
